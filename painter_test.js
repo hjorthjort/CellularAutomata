998b1678ae8ae45ae70e9d9eaf728820982eb4b2
hjorthjort <rikard.hjort@gmail.com>
2016-09-04 20:52:11 +0200

    Testing tools

--- painter_test.js
+++ painter_test.js
@@ -1,6 +1,17 @@
 // Testing tools
 ca.testing = {};
 
+ca.testing.AssertionError = Error;
+ca.testing.assert = function(assertion, opt_message) {
+    if (!assertion)
+        throw new ca.testing.AssertionError(opt_message);
+}
+
+ca.testing.assertEqual = function(obj1, obj2, opt_message) {
+    if (obj1 != obj2 || (obj1.equals && obj1.equals(obj2)))
+        throw new ca.testing.AssertionError(opt_message);
+}
+
 // Unit tests
 ca.testing.tests = {};
 
