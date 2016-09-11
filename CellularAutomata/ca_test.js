b8b2003e7baaf90ddeb207c5149d9d47b54d76fb
hjorthjort <rikard.hjort@gmail.com>
2016-09-11 21:24:26 +0200

    Move automaton related tests to separate module

---
+++ CellularAutomata/ca_test.js
@@ -0,0 +1,65 @@
+ca.tests = {};
+
+// Run all automaton tests
+ca.tests.automaton_tests = function() {
+    return main.testing.run_tests(ca.tests.automaton_tests.tests, "AutomatonTests");
+};
+
+ca.tests.automaton_tests.tests = {};
+
+// Test the correct implementation of the rule generator
+ca.tests.automaton_tests.tests.test_rule_generator = function() {
+    var aux = function(expected_results, generator) {
+        for (var i = 0; i < 8; i++)
+            main.testing.assertEqual(expected_results[i], generator(i >> 2, (i >> 1) % 2, i % 2), "Fail");
+    }
+    // Rule 0
+    var generator = new ca.RuleGenerator(0);
+    var results = [0, 0, 0, 0, 0, 0, 0, 0];
+    aux(results, generator);
+
+    // Rule 30
+    var generator = new ca.RuleGenerator(30);
+    var results = [0, 1, 1, 1, 1, 0, 0, 0];
+    aux(results, generator);
+
+    // Rule 110
+    var generator = new ca.RuleGenerator(110);
+    var results = [0, 1, 1, 1, 0, 1, 1, 0];
+    aux(results, generator);
+
+    // Rule 110
+    var generator = new ca.RuleGenerator(250);
+    var results = [0, 1, 0, 1, 1, 1, 1, 1];
+    aux(results, generator);
+
+    return true;
+};
+
+ca.tests.automaton_tests.tests.next_state_test = function() {
+    var verify_identical = function(array1, array2) {
+        if (array1.length != array2.length)
+            return false;
+        for (var i = 0; i < array1.length; i++)
+            if (array1[i] != array2[i])
+                return false;
+        return true;
+    }
+    var rule10correct = [
+        [0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0],
+    ];
+    var automaton = new ca.Automaton(new ca.RuleGenerator(10), rule10correct[0]);
+    for (var i = 0; i < rule10correct.length; i++) {
+        if (!verify_identical(rule10correct[i], automaton.getState()))
+            return false;
+        automaton.nextState();
+    }
+    return true;
+};
+
+
+// Run all tests.
+main.testing.run_module_tests(ca.tests, "Cellular Automata tests");
